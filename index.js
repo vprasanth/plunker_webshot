0f8e859cf0fbd5b8e7509a41d744ef4d70b8cd28
Prasanth Vaaheeswaran <pvaahee@ca.ibm.com>
2015-11-25 17:55:03 -0500

    Server no longer explodes on malformed cookie.

--- index.js
+++ index.js
@@ -1,92 +1,115 @@
-var Boom = require("boom");
-var Concat = require("concat-stream");
-var Hapi = require("hapi");
-var Image = require("imagemagick-stream");
-var Joi = require("joi");
-var LRU = require("bluebird-lru-cache");
-var Promise = require("bluebird");
-var Screenshot = require("screenshot-stream");
+/*global require, process*/
+(function () {
+  'use strict';
 
+  var Boom = require('boom');
+  var Concat = require('concat-stream');
+  var Hapi = require('hapi');
+  var Image = require('imagemagick-stream');
+  var Joi = require('joi');
+  var LRU = require('bluebird-lru-cache');
+  var Promise = require('bluebird');
+  var Screenshot = require('screenshot-stream');
+  var nconf = require('nconf');
 
-if (!process.env.RUN_URL) throw new Error("RUN_URL environment variable is required.");
-if (!process.env.PORT) throw new Error("PORT environment variable is required.");
+  nconf
+    .argv()
+    .env()
+    .file({file: 'config.json'});
 
-var server = new Hapi.Server(process.env.HOST || "0.0.0.0", process.env.PORT);
+  if (!nconf.get('runURL')) {
+    throw new Error('runURL environment variable is required.');
+  }
+  if (!nconf.get('port')) {
+    throw new Error('port environment variable is required.');
+  }
 
+  var runURL = nconf.get('runURL');
+  var port = nconf.get('port');
 
-setTimeout(function () {
-  server.log('info', 'Server exceeded maximum lifetime, exiting.');
-  process.exit(0);
-}, 1000 * 60 * 60);
-
-
-var internals = {};
-
-internals.runUrl = process.env.RUN_URL;
-
-internals.prepareShot = function (key) {
-  var plunkId = key.split("@")[0];
-  
-  return new Promise(function (resolve, reject) {
-    var captureStream = Screenshot(internals.runUrl + "/plunks/" + plunkId + "/", "1024x768", {delay: 2});
-    var resizeStream = Image().resize("248").gravity("NorthWest").crop("248x372").quality(75);
-    var concatStream = Concat(function (buf) {
-      if (!buf.length) {
-        return reject(Boom.serverTimeout("Invalid preview, empty buffer"));
+  var server = new Hapi.Server(process.env.HOST || '0.0.0.0', port, {
+    state: {
+      cookies: {
+        failAction: 'log'
       }
-      
-      resolve(buf);
-    });
-    
-    captureStream
-      .pipe(resizeStream)
-      .pipe(concatStream)
-      .on("error", reject);
-  });
-};
-
-
-internals.cache = LRU({
-  max: 1024 * 1024 * 256,
-  length: function (buf) { return buf.length; },
-  fetchFn: internals.prepareShot
-});
-
-server.route({
-  method: "GET",
-  path: "/{plunkId}.png",
-  config: {
-    validate: {
-      params: {
-        plunkId: Joi.string().alphanum().required()
-      },
-      query: {
-        d: Joi.string().required()
-      }
-    },
-    handler: function (request, reply) {
-      internals.cache.get(request.params.plunkId + "@" + request.query.d)
-        .then(function (buf) {
-          reply(buf).type("image/png");
-        }, reply);
     }
-  }
-});
-
-server.pack.register({
-  plugin: require("good")
-  // options: {
-  //   subscribers: {
-  //     'console': [],
-  //     '/tmp/webshot/': ['request', 'log', 'error'],
-  //   },
-  // },
-}, function (err) {
-  if (err) {
-    throw err; // something bad happened loading the plugin
-  }
-
-  server.start(function () {
-    server.log('info', 'Server running at: ' + server.info.uri);
   });
-});
+
+  setTimeout(function () {
+    server.log('info', 'Server exceeded maximum lifetime, exiting.');
+    process.exit(0);
+  }, 1000 * 60 * 60);
+
+  var internals = {};
+
+  internals.runUrl = runURL;
+
+  internals.prepareShot = function (key) {
+    var plunkId = key.split('@')[0];
+
+    return new Promise(function (resolve, reject) {
+      var captureStream = Screenshot(internals.runUrl + '/plunks/' + plunkId + '/', '1024x768', {delay: 2});
+      var resizeStream = Image().resize('248').gravity('NorthWest').crop('248x372').quality(75);
+      var concatStream = Concat(function (buf) {
+        if (!buf.length) {
+          return reject(Boom.serverTimeout('Invalid preview, empty buffer'));
+        }
+
+        resolve(buf);
+      });
+
+      captureStream
+        .pipe(resizeStream)
+        .pipe(concatStream)
+        .on('error', reject);
+    });
+  };
+
+
+  internals.cache = LRU({
+    max: 1024 * 1024 * 256,
+    length: function (buf) { return buf.length; },
+    fetchFn: internals.prepareShot
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/{plunkId}.png',
+    config: {
+      validate: {
+        params: {
+          plunkId: Joi.string().alphanum().required()
+        },
+        query: {
+          d: Joi.string().required()
+        }
+      },
+      handler: function (request, reply) {
+        internals.cache.get(request.params.plunkId + '@' + request.query.d)
+          .then(function (buf) {
+            reply(buf).type('image/png');
+          }, reply);
+      }
+    }
+  });
+
+  server.pack.register({
+    plugin: require('good')
+    // options: {
+    //   subscribers: {
+    //     'console': [],
+    //     '/tmp/webshot/': ['request', 'log', 'error'],
+    //   },
+    // },
+  }, function (err) {
+    if (err) {
+      throw err; // something bad happened loading the plugin
+    }
+
+    server.start(function () {
+      server.log('info', 'Server running at: ' + server.info.uri);
+    });
+  });
+
+})();
+
